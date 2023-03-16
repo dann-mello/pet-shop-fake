c90e695d9ad83aa420575efdce7d27be6bf455a0
Daniel Melo <o.danielworkmelo@gmail.com>
2023-03-16 08:15:25 +0000

    Estrutura de controles e rotas criada

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { resourceLimits } from "worker_threads";
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/searchController';
+
+const router = Router();
+
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('/', SearchController.search);
+
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
+import { setServers } from 'dns';
 
 //Variaveis de ambiente
 dotenv.config();
@@ -16,5 +18,11 @@ server.engine('mustache', mustache());
 server.use(express.static(path.join(__dirname, '../public')));
 
 //Rotas
+server.use(mainRoutes);
+server.use((req, res) => {
+    res.send('Página não encontrada!')
+})
+
+
 
 server.listen(process.env.PORT);
